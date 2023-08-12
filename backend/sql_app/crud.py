f2a7af6d23421f9e8aa70771cb4a7af7c4c04cc4
tdbui1209 <tdbui1209@gmail.com>
2023-08-12 23:48:18 +0700

    fix typo Categories, Products

--- backend/sql_app/crud.py
+++ backend/sql_app/crud.py
@@ -4,11 +4,11 @@ from . import models, schemas
 
 
 def get_categories(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Category).offset(skip).limit(limit).all()
+    return db.query(models.Categories).offset(skip).limit(limit).all()
 
 
 def create_category(db: Session, category: schemas.CategoryBase):
-    db_category = models.Category(name=category.name)
+    db_category = models.Categories(name=category.name)
     db.add(db_category)
     db.commit()
     db.refresh(db_category)
@@ -16,11 +16,11 @@ def create_category(db: Session, category: schemas.CategoryBase):
 
 
 def get_products(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Product).offset(skip).limit(limit).all()
+    return db.query(models.Products).offset(skip).limit(limit).all()
 
 
 def create_products(db: Session, product: schemas.ProductBase):
-    db_product = models.Product(
+    db_product = models.Products(
         name=product.name,
         price=product.price,
         description=product.description,
