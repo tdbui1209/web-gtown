9c7f984554c94538404188c6cfdc4cd4025462d5
tdbui1209 <tdbui1209@gmail.com>
2023-08-12 23:36:03 +0700

    add create category api

--- backend/sql_app/main.py
+++ backend/sql_app/main.py
@@ -19,3 +19,11 @@ async def root():
 async def read_categories(skip: int = 0, limit: int = 100, db: Session = Depends(SessionLocal)):
     categories = crud.get_categories(db, skip=skip, limit=limit)
     return categories
+
+
+@app.post("/categories/", response_model=schemas.Category)
+async def create_category(category: schemas.CategoryBase, db: Session = Depends(SessionLocal)):
+    db_category = crud.create_category(db, category=category)
+    if db_category is None:
+        raise HTTPException(status_code=400, detail="Category already exists")
+    return db_category
