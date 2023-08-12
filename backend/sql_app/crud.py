316c6b05cba95b4cb1615e778b1f5c8d1ec9d73e
tdbui1209 <tdbui1209@gmail.com>
2023-08-12 23:28:14 +0700

    add create_products crud

--- backend/sql_app/crud.py
+++ backend/sql_app/crud.py
@@ -17,3 +17,16 @@ def create_category(db: Session, category: schemas.CategoryBase):
 
 def get_products(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Product).offset(skip).limit(limit).all()
+
+
+def create_products(db: Session, product: schemas.ProductBase):
+    db_product = models.Product(
+        name=product.name,
+        price=product.price,
+        description=product.description,
+        category_id=product.category_id,
+    )
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
