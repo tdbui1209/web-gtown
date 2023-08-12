ed2c1ca31d51080e28e7f4c85abc8daafa56b8e2
tdbui1209 <tdbui1209@gmail.com>
2023-08-12 23:25:35 +0700

    add get_categories

--- backend/sql_app/crud.py
+++ backend/sql_app/crud.py
@@ -1,3 +1,7 @@
 from sqlalchemy.orm import Session
 
 from . import models, schemas
+
+
+def get_categories(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Category).offset(skip).limit(limit).all()
