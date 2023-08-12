f08894c8f3ab91c3b3eacc51004a38a1d54098e7
tdbui1209 <tdbui1209@gmail.com>
2023-08-12 23:16:50 +0700

    add Categories model

--- backend/sql_app/models.py
+++ backend/sql_app/models.py
@@ -3,3 +3,9 @@ from sqlalchemy.orm import relationship
 
 from .database import Base
 
+
+class Categories(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
