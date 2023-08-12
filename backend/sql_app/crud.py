b1ca8ae18f29e91b7dd423ff95ff9568ab8b2cd9
tdbui1209 <tdbui1209@gmail.com>
2023-08-12 23:27:03 +0700

    add create_category crud

--- backend/sql_app/crud.py
+++ backend/sql_app/crud.py
@@ -5,3 +5,11 @@ from . import models, schemas
 
 def get_categories(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Category).offset(skip).limit(limit).all()
+
+
+def create_category(db: Session, category: schemas.CategoryBase):
+    db_category = models.Category(name=category.name)
+    db.add(db_category)
+    db.commit()
+    db.refresh(db_category)
+    return db_category
