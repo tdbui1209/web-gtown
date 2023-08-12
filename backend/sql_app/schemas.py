f71586e917d8a611a4485fd8ea193e8fe3adb5c8
tdbui1209 <tdbui1209@gmail.com>
2023-08-12 23:21:35 +0700

    add Product schema

--- backend/sql_app/schemas.py
+++ backend/sql_app/schemas.py
@@ -4,8 +4,23 @@ from pydantic import BaseModel
 class CategoryBase(BaseModel):
     name: str
 
+
 class Category(CategoryBase):
     id: int
+
+    class Config:
+        from_attributes = True
+
+
+class ProductBase(BaseModel):
+    name: str
+    price: float
+    description: str
+
+
+class Product(ProductBase):
+    id: int
+    category_id: int
 
     class Config:
         from_attributes = True
