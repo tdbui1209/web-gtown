f40de151f1f49012f6a4386a7a7763100e868a00
tdbui1209 <tdbui1209@gmail.com>
2023-08-12 23:31:58 +0700

    add create_all engine

--- backend/sql_app/main.py
+++ backend/sql_app/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
