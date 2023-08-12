bf60d063a7f5c0d21fe77927deaf182eed6b0d87
tdbui1209 <tdbui1209@gmail.com>
2023-08-12 23:33:53 +0700

    add get categories api

--- backend/sql_app/main.py
+++ backend/sql_app/main.py
@@ -13,3 +13,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/categories/", response_model=list[schemas.Category])
+async def read_categories(skip: int = 0, limit: int = 100, db: Session = Depends(SessionLocal)):
+    categories = crud.get_categories(db, skip=skip, limit=limit)
+    return categories
