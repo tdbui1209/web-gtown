c9829b8e45643f54f97bde004bcf709fdb63cdc4
tdbui1209 <tdbui1209@gmail.com>
2023-08-16 00:50:40 +0700

    add create_user crud

--- backend/sql_app/crud.py
+++ backend/sql_app/crud.py
@@ -34,3 +34,16 @@ def create_products(db: Session, product: schemas.ProductBase):
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Users).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserBase):
+    db_user = models.Users(
+        name=user.name,
+        email=user.email,
+        password=user.password,
+        status_id=1  # default status_id = 1
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
