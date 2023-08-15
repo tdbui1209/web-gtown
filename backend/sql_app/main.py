77634c8c1f27d11b906af3d7e782b7f22fdc630c
tdbui1209 <tdbui1209@gmail.com>
2023-08-16 00:56:16 +0700

    add create users api

--- backend/sql_app/main.py
+++ backend/sql_app/main.py
@@ -55,3 +55,11 @@ async def create_products(product: schemas.ProductBase, db: Session = Depends(ge
 async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
+
+
+@app.post("/users/", response_model=schemas.User)
+async def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
+    db_user = crud.create_user(db, user=user)
+    if db_user is None:
+        raise HTTPException(status_code=400, detail="User already exists")
+    return db_user
