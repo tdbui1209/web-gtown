928c625aad2e160cfb7f9182ea8790a5e4a679f7
tdbui1209 <tdbui1209@gmail.com>
2023-08-19 04:33:07 +0700

    add Status schemas

---
+++ backend/sql_app/schemas/statuses.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class StatusBase(BaseModel):
+    name: Optional[str] = None
+
+
+class StatusCreate(StatusBase):
+    name: str
+
+
+class StatusInDBBase(StatusBase):
+    id: int
+    name: str
+
+    class Config:
+        orm_mode = True
