df5e9e5401c9aa40fcfcc8408bf9c7c73c6327b0
tdbui1209 <tdbui1209@gmail.com>
2023-08-19 03:51:32 +0700

    refactor categories schemas

--- backend/sql_app/schemas/categories.py
+++ backend/sql_app/schemas/categories.py
@@ -1,9 +1,18 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
 class CategoryBase(BaseModel):
+    name: Optional[str] = None
+
+
+class CategoryCreate(CategoryBase):
     name: str
 
 
-class Category(CategoryBase):
+class CategoryInDBBase(CategoryBase):
     id: int
+    name: str
+
+    class Config:
+        orm_mode = True
