7bf8350290169f706bedc6872a98827e0b962e4f
tdbui1209 <tdbui1209@gmail.com>
2023-08-19 17:48:45 +0700

    add users schemas

---
+++ backend/sql_app/schemas/users.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    username: str
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserInDBBase(UserBase):
+    username: str
+    name: str
+    email: str
+    password: str
+    status_id: int
+
+    class Config:
+        from_attributes = True
