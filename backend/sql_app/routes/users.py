e6bd1688d0ecb82f8a248df40446f360506c1780
tdbui1209 <tdbui1209@gmail.com>
2023-08-19 18:49:13 +0700

    add create users api

--- backend/sql_app/routes/users.py
+++ backend/sql_app/routes/users.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from database.connection import get_session
-from models.models import Users
+from models.models import Users, Status
 from schemas import users
 
 
@@ -15,3 +15,14 @@ user_router = APIRouter(
 @user_router.get("/", response_model=list[users.UserBase])
 async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_session)):
     return db.query(Users).offset(skip).limit(limit).all()
+
+
+@user_router.post("/", response_model=users.UserInDBBase)
+async def create_user(user: users.UserCreate, db: Session = Depends(get_session)):
+    active_status_id = db.query(Status).filter(Status.name == "active").one().id
+    db_user = Users(username=user.username, name=user.name, email=user.email,
+                    password=user.password, status_id=active_status_id)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
