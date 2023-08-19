21820bcb87ce1f5e00c6e77d9d971fb1e2a39a43
tdbui1209 <tdbui1209@gmail.com>
2023-08-19 17:47:58 +0700

    remove not used modudle

--- backend/sql_app/database/connection.py
+++ backend/sql_app/database/connection.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from models import models
 
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost:5432/gameshop"
