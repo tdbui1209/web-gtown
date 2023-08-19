03ee89abf24e8cfae085ae9f17b8c1947f850aeb
tdbui1209 <tdbui1209@gmail.com>
2023-08-19 17:49:16 +0700

    add read users api

---
+++ backend/sql_app/routes/users.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+from database.connection import get_session
+from models.models import Users
+from schemas import users
+
+
+user_router = APIRouter(
+    prefix="/users",
+    tags=["users"],
+)
+
+
+@user_router.get("/", response_model=users.BaseModel)
+async def read_users(skip: int = 0, limit: int = 0, db: Session = Depends(get_session)):
+    return db.query(Users).offset(skip).limit(limit).all()
