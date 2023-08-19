c0278714183e3785c3c8c6deb79c2533cfca3640
tdbui1209 <tdbui1209@gmail.com>
2023-08-19 16:13:42 +0700

    change config key orm_mode to from_attributes

--- backend/sql_app/schemas/categories.py
+++ backend/sql_app/schemas/categories.py
@@ -15,4 +15,4 @@ class CategoryInDBBase(CategoryBase):
     name: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
--- backend/sql_app/schemas/statuses.py
+++ backend/sql_app/schemas/statuses.py
@@ -15,4 +15,4 @@ class StatusInDBBase(StatusBase):
     name: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
