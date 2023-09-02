6776fc79af5a54578202e31e3bdabdc91916790e
tdbui1209 <tdbui1209@gmail.com>
2023-09-03 03:17:04 +0700

    add prodcut schemas

--- backend/app/schemas/sche_product.py
+++ backend/app/schemas/sche_product.py
@@ -5,22 +5,18 @@ from pydantic import BaseModel
 
 
 class ProductBase(BaseModel):
-    pass
+    product_name: str
+    product_price: int
+    product_description: Optional[str] = None
+    product_category_id: int
 
     class Config:
         from_attribute = True
 
 
 class ProductCreate(ProductBase):
-    product_name: str
-    product_price: int
-    product_description: Optional[str] = None
-    product_category_id: int
+    pass
 
 
 class ProductItem(ProductBase):
     id: int
-    product_name: str
-    product_price: int
-    product_description: Optional[str] = None
-    product_category_id: int
