ed2e6c0a0ed3c7cd1a72a21df0bcf37cc7dfb731
tdbui1209 <tdbui1209@gmail.com>
2023-09-02 18:34:19 +0700

    add product model

---
+++ backend/app/models/model_product.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, ForeignKey, String
+
+from .model_base import Base
+
+
+class Products(Base):
+    product_name = Column(String(100), nullable=False)
+    product_price = Column(Integer, nullable=False)
+    product_description = Column(String(200), nullable=True)
+    product_category_id = Column(Integer, ForeignKey('productcategories.id'))
