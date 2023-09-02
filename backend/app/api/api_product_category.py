7d3e26b33ddd67cda7bad9844a9f98172fa6323e
tdbui1209 <tdbui1209@gmail.com>
2023-09-02 18:36:31 +0700

    add product category api

---
+++ backend/app/api/api_product_category.py
@@ -0,0 +1,32 @@
+import logging
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi_sqlalchemy import db
+
+from schemas.sche_product_category import ProductCategoryCreate, ProductCategoryItem
+from models.model_product_category import ProductCategories
+
+
+logger = logging.getLogger()
+router = APIRouter()
+
+
+@router.get('/', response_model=list[ProductCategoryItem])
+def read_product_categories():
+    try:
+        product_categories = db.session.query(ProductCategories).all()
+        return product_categories
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=logger.error(e))
+    
+
+@router.post('/', response_model=ProductCategoryCreate)
+def create_product_category(product_category: ProductCategoryCreate):
+    try:
+        db_category = ProductCategories(
+            product_category_name=product_category.product_category_name
+        )
+        db.session.add(db_category)
+        db.session.commit()
+        return product_category
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=logger.error(e))
