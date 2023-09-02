e06ad81699fda3d22d587ffca9b2cb532d80f24d
tdbui1209 <tdbui1209@gmail.com>
2023-09-03 03:19:22 +0700

    register product api

--- backend/app/api/api_router.py
+++ backend/app/api/api_router.py
@@ -1,12 +1,18 @@
 from fastapi import APIRouter
 
-from . import api_product_category
+from . import api_product_category, api_product
 
 
 router = APIRouter()
 
 router.include_router(
     api_product_category.router,
-    prefix="/product_category",
-    tags=["product_category"]
+    prefix="/product_categories",
+    tags=["product_categories"]
+)
+
+router.include_router(
+    api_product.router,
+    prefix="/products",
+    tags=["products"]
 )
