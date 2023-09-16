cd6272cc510a3514c614403b2a41d93af400e7be
tdbui1209 <tdbui1209@gmail.com>
2023-09-17 00:37:37 +0700

    register user api

--- backend/app/api/api_router.py
+++ backend/app/api/api_router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api import api_healthcheck, api_product
+from app.api import api_healthcheck, api_product, api_user
 
 
 router = APIRouter()
 
 router.include_router(api_healthcheck.router, prefix="/healthcheck", tags=["health-check"])
 router.include_router(api_product.router, prefix="/products", tags=["products"])
+router.include_router(api_user.router, prefix="/users", tags=["users"])
