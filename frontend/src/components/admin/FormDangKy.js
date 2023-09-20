e05fb115d9de51ac4ddd0bf4d78a07161c1ff422
phphuc0608 <phuc31082002@gmail.com>
2023-09-20 10:41:08 +0700

    fix bug responsive navbar

--- frontend/src/components/admin/FormDangKy.js
+++ frontend/src/components/admin/FormDangKy.js
@@ -13,11 +13,11 @@ const FormDangKy = () => {
       <div className="row d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
         <div className="col-md-12 p-5" id="sign_up_container">
           <div className="col-md-12 p-3">
-            <h2 style={{ color: "white" }} className="p-2">Sign Up</h2>
+            <h2 style={{ color: "white" }} className="p-2">ĐĂNG KÝ</h2>
           </div>
           <div className="col-md-6 float-left">
             <div className="form-group">
-              <label htmlFor="user_name" style={{ color: "white" }}>User name</label><br />
+              <label htmlFor="user_name" style={{ color: "white" }}>Tên tài khoản</label><br />
               <input type="text" className="w-100 p-1" id="user_name" />
             </div>
             <div className="form-group p-1">
@@ -25,25 +25,25 @@ const FormDangKy = () => {
               <input type="text" className="w-100 p-1" id="email" />
             </div>
             <div className="form-group p-1">
-              <label htmlFor="phone_number" style={{ color: "white" }}>Phone Number</label><br />
+              <label htmlFor="phone_number" style={{ color: "white" }}>Số điện thoại</label><br />
               <input type="text" className="w-100 p-1" id="phone_number" />
             </div>
           </div>
           <div className="col-md-6 float-left">
             <div className="form-group p-1">
-              <label htmlFor="password" style={{ color: "white" }}>Password</label><br />
+              <label htmlFor="password" style={{ color: "white" }}>Mật khẩu</label><br />
               <input type="password" className="w-100 p-1" id="password" />
             </div>
             <div className="form-group p-1">
-              <label htmlFor="confirm_password" style={{ color: "white" }}>Confirm Password</label><br />
+              <label htmlFor="confirm_password" style={{ color: "white" }}>Xác nhận mật khẩu</label><br />
               <input type="password" className="w-100 p-1" id="confirm_password" />
             </div>
             <div className="form-group p-1">
               <input type="checkbox" />
-              <a href="#" className="link px-2">I accept the terms and condition</a>
+              <a href="#" className="link px-2">Tôi đồng ý với các quy định và chính sách</a>
             </div>
             <div className="form-group p-2">
-              <button className="font-weight-bold button_blue align-items-center py-1 px-5 w-40" style={{ color: "white" }}>SIGN UP</button>
+              <button className="font-weight-bold button_blue align-items-center py-1 px-5 w-40" style={{ color: "white" }}>ĐĂNG KÝ</button>
             </div>
           </div>
         </div>
--- frontend/src/components/admin/Navbar.js
+++ frontend/src/components/admin/Navbar.js
@@ -6,33 +6,35 @@ import '../css/admin/style.css';
 import Logo from "../../assets/img/logoGtown.png";
 const Navbar = () => {
   const navRef = useRef();
-  // const showNavBar = () => {
-  //   if (window.innerWidth <= 1024) {
-  //     navRef.current.classList.toggle("responsive_nav");
-  //   }
-  // }
   const showNavBar = () => {
-    navRef.current.classList.toggle("responsive_nav");
+    if (window.innerWidth <= 1024) {
+      navRef.current.classList.toggle("responsive_nav");
+      // console.log("Hi");
+    }
   }
-  const hideNavBar = () => {
-    navRef.current.classList.remove("responsive_nav");
+
+  const handleNavItemClick = () => {
+    if (window.innerWidth <= 1024) {
+      showNavBar(); 
+      // console.log(navRef.current.classList);
+    }
   }
   return(
     <header id="header">
         <img src={Logo} alt="adminLogo" className="logoShop"/>
         <nav id="nav" ref={navRef}>
-            <a href="/trang_chu" className="navItem font-weight-bold">TRANG CHỦ</a>
-            <a href="/quan_ly_loai_san_pham" className="navItem font-weight-bold">LOẠI SẢN PHẨM</a>
-            <a href="/quan_ly_san_pham" className="navItem font-weight-bold">SẢN PHẨM</a>
-            <a href="/quan_ly_nguoi_dung" className="navItem font-weight-bold">NGƯỜI DÙNG</a>
-            <a href="/quan_ly_nha_san_xuat" className="navItem font-weight-bold">NGƯỜI DÙNG</a>
-            <a href="/quan_ly_tin_tuc" className="navItem font-weight-bold">TIN TỨC</a>
-            <button className="nav_btn nav_close_btn" onClick={showNavBar}>
+            <Link href="/trang_chu" className="navItem font-weight-bold" onClick={handleNavItemClick}>TRANG CHỦ</Link>
+            <Link href="/quan_ly_loai_san_pham" className="navItem font-weight-bold" onClick={handleNavItemClick}>LOẠI SẢN PHẨM</Link>
+            <Link href="/quan_ly_san_pham" className="navItem font-weight-bold" onClick={handleNavItemClick}>SẢN PHẨM</Link>
+            <Link href="/quan_ly_nguoi_dung" className="navItem font-weight-bold" onClick={handleNavItemClick}>NGƯỜI DÙNG</Link>
+            <Link href="/quan_ly_nha_san_xuat" className="navItem font-weight-bold" onClick={handleNavItemClick}>NGƯỜI DÙNG</Link>
+            <Link href="/quan_ly_tin_tuc" className="navItem font-weight-bold" onClick={handleNavItemClick}>TIN TỨC</Link>
+            <button className="nav_close_btn" onClick={showNavBar}>
               <i className="bi bi-x"></i>
             </button>
         </nav>
         <div>
-          <button className="nav_btn" onClick={hideNavBar}>
+          <button className="nav_btn" onClick={showNavBar}>
             <i className="bi bi-list icon-admin"></i>
           </button>
           <button className="sign_out_btn">
